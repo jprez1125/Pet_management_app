2836e14acba264310f898a83172be544d6c41a1b
libanezdiaz <libanezdiaz@ivytech.edu>
2026-04-23 21:11:05 -0400

    Frontend progress -LB

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Backend basic model
+
+from django.db import models
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    pet_type = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
