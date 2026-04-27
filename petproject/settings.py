113027b333b2f8e5245f5d199745997582f92056
Kalisa <damasckh@amazon.com>
2026-04-27 15:50:49 -0400

    Prepare Django app for deployment

--- petproject/settings.py
+++ petproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=&!=*k7s4zn0l^do(6+)dt^&)(u#@*f+(3bh9e@)mcs9f3x@uu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,14 +42,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'petproject.urls'
