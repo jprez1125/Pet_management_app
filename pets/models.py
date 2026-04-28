094713a0830a8f53f213c9c10c6a6c5a4d467191
jprez1125 <144505315+jprez1125@users.noreply.github.com>
2026-04-27 22:01:30 -0400

    finished models, reran migrations-jp

--- pets/models.py
+++ pets/models.py
@@ -4,10 +4,20 @@ from django.db import models
 
 from django.db import models
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Pet(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name="pets")
     name = models.CharField(max_length=100)
     pet_type = models.CharField(max_length=50)
     age = models.IntegerField()
 
     def __str__(self):
         return self.name
+    
