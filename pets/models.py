0182be1815325a0a5dad96452c94ff922b2e40d0
dkalisa317-cell <dkalisa317@gmail.com>
2026-04-27 21:43:31 -0400

    Fix merge conflicts and run project

--- pets/models.py
+++ pets/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
-# Create your models here.
-=======
-# Backend basic model
-
-from django.db import models
-
 class Pet(models.Model):
     name = models.CharField(max_length=100)
     pet_type = models.CharField(max_length=50)
@@ -14,4 +7,3 @@ class Pet(models.Model):
 
     def __str__(self):
         return self.name
->>>>>>> 680c3799e202d6fe7d2e80845d5548655b17689b
--- pets/urls.py
+++ pets/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path("", views.home, name="home"),
     path("add-pet/", views.add_pet, name="add_pet"),
     path("add-appointment/", views.add_appointment, name="add_appointment"),
-=======
-    path('', views.pet_list, name='pet_list'),
->>>>>>> 680c3799e202d6fe7d2e80845d5548655b17689b
+    path("pets/", views.pet_list, name="pet_list"),
 ]
