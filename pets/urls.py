aaad3eab1edba86a0871d872f7a914a0e1820491
Kalisa <damasckh@amazon.com>
2026-05-05 11:20:11 -0400

    Update homepage appointment card link

--- pets/urls.py
+++ pets/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import home, add_pet, add_appointment, pet_list, edit_pet, delete_pet
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -9,7 +8,8 @@ urlpatterns = [
     path("pets/", views.pet_list, name="pet_list"),
     path("pets/<int:id>/edit/", views.edit_pet, name="edit_pet"),
     path("pets/<int:id>/delete/", views.delete_pet, name="delete_pet"),
+
     path("appointments/", views.appointment_list, name="appointment_list"),
+    path("appointments/<int:id>/edit/", views.edit_appointment, name="edit_appointment"),
     path("appointments/<int:id>/delete/", views.delete_appointment, name="delete_appointment"),
-    path ("appointments/<int:id>/edit/", views.edit_appointment, name="edit_appointment"),
 ]
