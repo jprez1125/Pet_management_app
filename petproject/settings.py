bc5091ccfab390d3208e43a6fccdf304698d1770
Kalisa <damasckh@amazon.com>
2026-05-06 08:29:21 -0400

    Add login and protect management pages

--- petproject/settings.py
+++ petproject/settings.py
@@ -87,3 +87,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- pets/views.py
+++ pets/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 
-from pets.pet_form import PetForm
-from .models import Pet
-from .pet_form import AppointmentForm
-from .models import Appointment
+from .models import Pet, Appointment
+from .pet_form import PetForm, AppointmentForm
+
 
 def home(request):
     return render(request, "pets/home.html")
 
+
+@login_required
 def add_pet(request):
     if request.method == "POST":
         form = PetForm(request.POST)
@@ -17,65 +19,90 @@ def add_pet(request):
     else:
         form = PetForm()
 
-    return render(request, "pets/add_pet.html", {"form": form}) 
+    return render(request, "pets/add_pet.html", {"form": form})
 
 
+@login_required
 def add_appointment(request):
     if request.method == "POST":
         form = AppointmentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("pet_list")  # temporary redirect
+            return redirect("appointment_list")
     else:
         form = AppointmentForm()
 
-    return render(request, "pets/add_appointment.html", {"form": form}) 
+    return render(request, "pets/add_appointment.html", {"form": form})
+
 
 def pet_list(request):
-    pets = Pet.objects.all().prefetch_related('appointments')
-    return render(request, 'pets/pet_list.html', {'pets': pets})
+    pets = Pet.objects.all().prefetch_related("appointments")
+    return render(request, "pets/pet_list.html", {"pets": pets})
 
+
+@login_required
 def edit_pet(request, id):
     pet = get_object_or_404(Pet, id=id)
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = PetForm(request.POST, instance=pet)
         if form.is_valid():
-            pet.save()
-            return redirect ( 'edit_pet', id=pet.id)
+            form.save()
+            return redirect("pet_list")
     else:
         form = PetForm(instance=pet)
+
     appointments = pet.appointments.all()
-    return render(request, 'pets/pet_form.html', {
-        'form': form,
-        'pet': pet,
-        'appointments': appointments
+
+    return render(request, "pets/pet_form.html", {
+        "form": form,
+        "pet": pet,
+        "appointments": appointments
     })
 
+
+@login_required
 def delete_pet(request, id):
     pet = get_object_or_404(Pet, id=id)
-    if request.method == 'POST':
+
+    if request.method == "POST":
         pet.delete()
-        return redirect('pet_list')
+        return redirect("pet_list")
+
+    return render(request, "pets/delete_pet.html", {"pet": pet})
+
 
 def appointment_list(request):
     appointments = Appointment.objects.all()
-    return render(request, 'pets/appointment_list.html', {'appointments': appointments})
+    return render(request, "pets/appointment_list.html", {"appointments": appointments})
 
-def delete_appointment(request, id):
-    appt = get_object_or_404(Appointment, id=id)
-    if request.method == "POST":
-        appt.delete()
-    return redirect('pet_list')
 
+@login_required
 def edit_appointment(request, id):
     appt = get_object_or_404(Appointment, id=id)
+
     if request.method == "POST":
         form = AppointmentForm(request.POST, instance=appt)
         if form.is_valid():
             form.save()
-            return redirect('appointment_list')
+            return redirect("appointment_list")
     else:
-        form = AppointmentForm(instance=Appointment)
-    return render(request, 'pets/edit_appointment.html', {'form': form})
+        form = AppointmentForm(instance=appt)
+
+    return render(request, "pets/edit_appointment.html", {
+        "form": form,
+        "appointment": appt
+    })
 
 
+@login_required
+def delete_appointment(request, id):
+    appt = get_object_or_404(Appointment, id=id)
+
+    if request.method == "POST":
+        appt.delete()
+        return redirect("appointment_list")
+
+    return render(request, "pets/delete_appointment.html", {
+        "appointment": appt
+    })
